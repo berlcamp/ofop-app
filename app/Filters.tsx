5da72ef4ab987b336a4c39a5f8e0f3c933d7f63e
berlcamp <berlcamp@gmail.com>
2023-08-25 16:22:38 +0800

    ..

--- app/Filters.tsx
+++ app/Filters.tsx
@@ -2,38 +2,38 @@ import React, { Fragment, useEffect, useState } from 'react'
 import { Listbox, Transition } from '@headlessui/react'
 import { CheckIcon, ChevronDownIcon, MagnifyingGlassIcon, TagIcon } from '@heroicons/react/20/solid'
 import { CustomButton } from '@/components'
+import { barangays } from '@/constants/TrackerConstants'
 import uuid from 'react-uuid'
 
 interface FilterTypes {
   setFilterKeyword: (keyword: string) => void
   setFilterBarangay: (brgy: string) => void
+  setFilterRequirements: (brgy: string) => void
 }
 
-const barangays = [
-  '50TH',
-  'AGUADA'
-]
-
-const Filters = ({ setFilterKeyword, setFilterBarangay }: FilterTypes) => {
+const Filters = ({ setFilterKeyword, setFilterBarangay, setFilterRequirements }: FilterTypes) => {
   const [keyword, setKeyword] = useState<string>('')
   const [barangay, setBarangay] = useState<string>('')
+  const [requirements, setRequirements] = useState<string>('')
 
   const handleApply = () => {
-    if (keyword.trim() === '' && barangay.trim() === '') return
+    if (keyword.trim() === '' && barangay.trim() === '' && requirements.trim() === '') return
 
     // pass filter values to parent
     setFilterKeyword(keyword)
     setFilterBarangay(barangay)
+    setFilterRequirements(requirements)
   }
 
   const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault()
 
-    if (keyword.trim() === '' && barangay.trim() === '') return
+    if (keyword.trim() === '' && barangay.trim() === '' && requirements.trim() === '') return
 
     // pass filter values to parent
     setFilterKeyword(keyword)
     setFilterBarangay(barangay)
+    setFilterRequirements(requirements)
   }
 
   // clear all filters
@@ -42,26 +42,29 @@ const Filters = ({ setFilterKeyword, setFilterBarangay }: FilterTypes) => {
     setKeyword('')
     setFilterBarangay('')
     setBarangay('')
+    setFilterRequirements('')
+    setRequirements('')
   }
 
   return (
     <div className=''>
-      <div className='items-center space-x-2 space-y-1'>
-        <form onSubmit={handleSubmit} className='items-center inline-flex'>
-          <div className='app__filter_container'>
+      <div className='items-center space-x-2'>
+        <form onSubmit={handleSubmit} className='items-center space-y-2'>
+          <div className='app__filter_container inline-flex'>
             <MagnifyingGlassIcon className="w-4 h-4 mr-1"/>
             <input
-              placeholder='Search'
+              placeholder='Search Name'
               value={keyword}
               onChange={e => setKeyword(e.target.value)}
               className="app__filter_input"/>
           </div>
-          <div className='app__filter_container'>
-            <MagnifyingGlassIcon className="w-4 h-4 mr-1"/>
+          <div className='app__filter_container inline-flex'>
+            <span className='text-sm text-gray-500'>Barangay</span>
             <select
               value={barangay}
               onChange={e => setBarangay(e.target.value)}
               className="app__filter_input">
+                <option></option>
                 {
                   barangays.map((bar) => (
                     <option key={uuid()}>{bar}</option>
@@ -69,6 +72,17 @@ const Filters = ({ setFilterKeyword, setFilterBarangay }: FilterTypes) => {
                 }
             </select>
           </div>
+          {/* <div className='app__filter_container inline-flex'>
+            <span className='text-sm text-gray-500'>Requirements</span>
+            <select
+              value={requirements}
+              onChange={e => setRequirements(e.target.value)}
+              className="app__filter_input">
+                <option></option>
+                <option>Complete</option>
+                <option>Incomplete</option>
+            </select>
+          </div> */}
         </form>
       </div>
       <div className='flex items-center space-x-2 mt-4'>
--- app/page.tsx
+++ app/page.tsx
@@ -5,18 +5,55 @@ import { PerPage, TopBarDark, TableRowLoading, ShowMore, Title } from '@/compone
 import Filters from './Filters'
 import { capitalizeWords, fullTextQuery } from '@/utils/text-helper'
 import { createClientComponentClient } from '@supabase/auth-helpers-nextjs'
+import { reqList } from '@/constants/TrackerConstants'
 
 // Redux imports
 import { useSelector, useDispatch } from 'react-redux'
 import { updateList } from '@/GlobalRedux/Features/listSlice'
 import { updateResultCounter } from '@/GlobalRedux/Features/resultsCounterSlice'
 import uuid from 'react-uuid'
+import { CheckCircleIcon, XMarkIcon } from '@heroicons/react/20/solid'
+
+interface RequirementTypes {
+  isComplete: boolean
+  label: string
+}
+
+const requirementsList: RequirementTypes[] = reqList
+
+const RequirementsCheckbox = ({ userId, req, userRequirements }: { userId: string, req: RequirementTypes, userRequirements: RequirementTypes[] }) => {
+  const [isChecked, setIsChecked] = useState(false)
+
+  useEffect(() => {
+    const userReq = userRequirements.filter((i: RequirementTypes) => {
+      if (i.label === req.label && i.isComplete) {
+        return true
+      } else {
+        return false
+      }
+    })
+
+    setIsChecked(userReq.length > 0)
+  }, [])
+
+  return (
+    <div key={uuid()} className='flex space-x-1 items-center'>
+      {
+        isChecked
+          ? <CheckCircleIcon className='w-4 h-4 text-green-500'/>
+          : <XMarkIcon className='w-4 h-4 text-red-500'/>
+      }
+      <span>{req.label}</span>
+    </div>
+  )
+}
 
 const Page: React.FC = () => {
   const [loading, setLoading] = useState(false)
   const [list, setList] = useState<any[]>([])
   const [filterKeyword, setFilterKeyword] = useState<string>('')
   const [filterBarangay, setFilterBarangay] = useState<string>('')
+  const [filterRequirements, setFilterRequirements] = useState<string>('')
   const [perPageCount, setPerPageCount] = useState<number>(10)
 
   const supabase = createClientComponentClient()
@@ -32,7 +69,7 @@ const Page: React.FC = () => {
     try {
       let query = supabase
         .from('ofop_scholars')
-        .select()
+        .select('*', { count: 'exact' })
 
       // Full text search
       if (filterKeyword !== '') {
@@ -41,6 +78,11 @@ const Page: React.FC = () => {
         searchSplit.forEach(item => {
           query = query.or(`firstname.ilike.%${item}%,middlename.ilike.%${item}%,lastname.ilike.%${item}%,fullname.ilike.%${item}%`)
         })
+      }
+
+      // Filter barangay
+      if (filterBarangay !== '') {
+        query = query.eq('barangay', filterBarangay)
       }
 
       // Per Page from context
@@ -78,11 +120,20 @@ const Page: React.FC = () => {
     try {
       let query = supabase
         .from('ofop_scholars')
-        .select()
+        .select('*', { count: 'exact' })
 
       // Full text search
       if (filterKeyword !== '') {
-        query = query.or(`firstname.ilike.%${filterKeyword}%,middlename.ilike.%${filterKeyword}%,lastname.ilike.%${filterKeyword}%,fullname.ilike.%${filterKeyword}%`)
+        const searchSplit = (filterKeyword).split(' ')
+        // Search match
+        searchSplit.forEach(item => {
+          query = query.or(`firstname.ilike.%${item}%,middlename.ilike.%${item}%,lastname.ilike.%${item}%,fullname.ilike.%${item}%`)
+        })
+      }
+
+      // Filter barangay
+      if (filterBarangay !== '') {
+        query = query.eq('barangay', filterBarangay)
       }
 
       // Per Page from context
@@ -125,7 +176,7 @@ const Page: React.FC = () => {
     void fetchData()
 
   // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [filterKeyword, perPageCount, filterBarangay])
+  }, [filterKeyword, perPageCount, filterBarangay, filterRequirements])
 
   const isDataEmpty = !Array.isArray(list) || list.length < 1 || !list
 
@@ -143,6 +194,7 @@ const Page: React.FC = () => {
             <Filters
               setFilterKeyword={setFilterKeyword}
               setFilterBarangay={setFilterBarangay}
+              setFilterRequirements={setFilterRequirements}
             />
           </div>
 
@@ -155,7 +207,7 @@ const Page: React.FC = () => {
 
           {/* Main Content */}
           <div>
-            <table className="app__table">
+          <table className="app__table">
               <thead className="app__thead">
                   <tr>
                       <th className="hidden md:table-cell app__th pl-4"></th>
@@ -163,13 +215,10 @@ const Page: React.FC = () => {
                           Student Name
                       </th>
                       <th className="hidden md:table-cell app__th">
-                          Barangay
+                          Completed Requirements
                       </th>
                       <th className="hidden md:table-cell app__th">
-                          Batch
-                      </th>
-                      <th className="hidden md:table-cell app__th">
-                          Contact #
+                          Remarks
                       </th>
                   </tr>
               </thead>
@@ -191,10 +240,31 @@ const Page: React.FC = () => {
                               : capitalizeWords(item.firstname + ' ' + item.middlename + ' ' + item.lastname)
                           }
                         </div>
+                        <div className='font-light'>No. {item.number}</div>
+                        <div className='font-light'>{item.contact_number}</div>
+                        <div className='font-light'>{item.school}</div>
+                        <div className='font-light'>{item.course}  {item.year}</div>
+                        <div className='font-light'>{item.barangay}</div>
                         {/* Mobile View */}
                         <div>
                           <div className="md:hidden app__td">
-                          Barangay: {item.barangay}
+                            <div>Requirements:</div>
+                            <div className='items-center mt-1'>
+                              {
+                                requirementsList.map((req: RequirementTypes) => (
+                                  <div key={uuid()} className='flex flex-col space-y-0'>
+                                    <RequirementsCheckbox
+                                      req={req}
+                                      userId={item.id}
+                                      userRequirements={item.requirements || []}
+                                      />
+                                  </div>
+                                ))
+                              }
+                            </div>
+                            <div className='mt-2'>
+                            Remarks: <span className='font-light'>{item.remarks || ''}</span>
+                            </div>
                           </div>
                         </div>
                         {/* End - Mobile View */}
@@ -202,15 +272,25 @@ const Page: React.FC = () => {
                       </th>
                       <td
                         className="hidden md:table-cell app__td">
-                        <div>{item.barangay}</div>
-                      </td>
-                      <td
-                        className="hidden md:table-cell app__td space-y-1">
-                        <div>{item.batch}</div>
+                        <div className='items-center'>
+                          {
+                            requirementsList.map((req: RequirementTypes) => (
+                              <div key={uuid()} className='flex flex-col space-y-0'>
+                                <RequirementsCheckbox
+                                  req={req}
+                                  userId={item.id}
+                                  userRequirements={item.requirements || []}
+                                  />
+                              </div>
+                            ))
+                          }
+                        </div>
                       </td>
                       <td
                         className="hidden md:table-cell app__td">
-                        <div>{item.contact_number}</div>
+                        <div>
+                          {item.remarks || ''}
+                        </div>
                       </td>
                     </tr>
                   ))
