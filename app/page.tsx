75e4961e2531861d9013dd0b9cc178412be9d960
berlcamp <berlcamp@gmail.com>
2023-08-30 09:24:15 +0800

    ..

--- app/page.tsx
+++ app/page.tsx
@@ -301,7 +301,7 @@ const Page: React.FC = () => {
                     </tr>
                   ))
                 }
-                { loading && <TableRowLoading cols={5} rows={2}/> }
+                { loading && <TableRowLoading cols={4} rows={2}/> }
               </tbody>
             </table>
             {
